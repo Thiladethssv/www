a597308349be90e6227d6038826fccdc36c45386
LAPTOP-GTOM5S0S\User <fildeth05@gmail.com>
2024-08-04 22:10:35 +0700

    my first commit

---
+++ js/login.js
@@ -0,0 +1,27 @@
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+    event.preventDefault(); 
+
+    
+    var validUsername = 'jxstin';
+    var validPassword = '1234';
+
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+    var errorMessage = document.getElementById('errorMessage');
+
+    if (username === validUsername && password === validPassword) {
+        
+       
+        window.location.href = 'home.html';
+    } 
+    
+    else {
+      Swal.fire({
+        icon: "error",
+        title: "Try again ",
+        text: "Username หรือ Password ไม่ถูกต้อง",
+      });
+        
+        errorMessage.textContent = 'Username หรือ Password ไม่ถูกต้อง';
+    }
+});
